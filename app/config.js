5680b3480bb3c84fd913a3b3e6173087f819b7b3
William Russell <william.russell@crownconsultinggroup.co.uk>
2022-09-08 16:45:22 +0100

    Commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Refined Talent Management',
+  serviceName: 'Talent Management',
 
   // Default port that prototype runs on
   port: '3000',
